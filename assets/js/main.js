d0b249a48f22b20e3533945275e3cf2e2a85dafa
petercrackthecode <nguyenvandung02051999@gmail.com>
2019-10-27 07:32:27 -0700

    more work

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,154 +1,202 @@
-
 (function($) {
+  var $window = $(window),
+    $body = $("body"),
+    $main = $("#main");
 
-	var	$window = $(window),
-		$body = $('body'),
-		$main = $('#main');
+  // Breakpoints.
+  breakpoints({
+    xlarge: ["1281px", "1680px"],
+    large: ["981px", "1280px"],
+    medium: ["737px", "980px"],
+    small: ["481px", "736px"],
+    xsmall: ["361px", "480px"],
+    xxsmall: [null, "360px"]
+  });
 
-	// Breakpoints.
-		breakpoints({
-			xlarge:   [ '1281px',  '1680px' ],
-			large:    [ '981px',   '1280px' ],
-			medium:   [ '737px',   '980px'  ],
-			small:    [ '481px',   '736px'  ],
-			xsmall:   [ '361px',   '480px'  ],
-			xxsmall:  [ null,      '360px'  ]
-		});
+  // Play initial animations on page load.
+  $window.on("load", function() {
+    window.setTimeout(function() {
+      $body.removeClass("is-preload");
+    }, 100);
+  });
 
-	// Play initial animations on page load.
-		$window.on('load', function() {
-			window.setTimeout(function() {
-				$body.removeClass('is-preload');
-			}, 100);
-		});
+  let $analyze = $("#analyze");
+
+  const handleResults = results => {
+    const readableResults = results.map(item => [item[0], item[1] * 100]);
+    readableResults.forEach(item => {
+      $("#table-analysis").append(
+        "<tr><td>" +
+          item[0] +
+          "</td><td>" +
+          Math.round(item[1] * 100) / 100 +
+          "%</td></tr>"
+      );
+    });
+
+    const getRandomColor = () => {
+      let letters = "0123456789ABCDEF";
+      let color = "#";
+
+      while (color === "#" || color === "#FFFFFF") {
+        color = "#";
+        for (let index = 0; index < 6; index++) {
+          color += letters[Math.floor(Math.random() * 15)];
+        }
+      }
+
+      return color;
+    };
+
+    const w = 1000;
+    const h = 200;
+    (function() {
+      let svg = d3
+        .select("strong")
+        .append("svg")
+        .attr("width", w + 'px')
+		.attr("height", h + 'px');
 		
 
-		let $analyze= $('#analyze');
+	  console.log('d3.select("strong")= ', d3.select('strong'));
+	  console.log(svg, " working?");
 
-		const handleResults = (results) => {
-			const readableResults = results.map(item => item[1] * 100)
-			readableResults.forEach(item => {
-				$("#table-analysis").append("<tr><td>" + item[0] + "</td><td>" + item[1] +"</td></tr>")
-			})
+        d3
+        .selectAll("rect")
+        .data(readableResults)
+        .enter()
+        .append("rect")
+        .attr("x", (d, i) => i * 30)
+        .attr("y", (d, i) => h - 3 * d[1])
+        .attr("width", 25)
+        .attr("height", (d, i) => d[1] * 3)
+        .attr("fill", getRandomColor());
 
-			d3.select("#data-analysis")
-			  .append('svg')
-			  .attr(width, '30px')
-			  .attr(height, data => data[1]);
-		}
+      let rect= svg
+        .selectAll("text")
+        .data(readableResults)
+        .enter()
+        .append("text")
+        .text(data => data[0])
+        .attr("x", (data, i) => i * 30)
+        .attr("y", (data, i) => h - 3 * data[1] - 3)
+        .attr("font-size", "25px")
+        .attr("color", "navy");
 
-		$('#analyze').on('submit', function(event) {
-			var accessToken = 'ya29.c.Kl6pBzSC_bRj0T0SITGaY_UZNHhh7Z39vQCkx7Z67ZxRdWXciKvk1kgHveqMh53p1MAcvkoOHT_9ZkL5XrZ0spgq3VtGwEfUnv8kqW46fLh8yo2vyHWxOZRitZ9cTAiU';
+	  console.log("Hi there", rect);
+    })();
+  };
 
-			event.preventDefault();
+  $("#analyze").on("submit", function(event) {
+    var accessToken =
+      "ya29.c.Kl6pB4-BCahTLLylyXHsZ6m2AeE6Q6yD9rUDdNHkO7yPzzg5WwPUizZYRoVarpg_8blv-Db7EP5N5t_-0jWfv23a9yNa8A4voJPxWqYrM8_XkThnlGN_QDWxERPZTqL8";
 
-			fetch("https://automl.googleapis.com/v1beta1/projects/795970644708/locations/us-central1/models/TCN490914349615939584:predict", {
-				body: JSON.stringify({
-						payload: { textSnippet: { content: $('#text-area').val(), mime_type: "text/plain" }}
-				}),
-				headers: {
-					Authorization: "Bearer " + accessToken,
-					"Content-Type": "application/json"
-				},
-				method: "POST"
-			}).then(res => res.json()).then(res => {
-				var results = res.payload.map(item => [item.displayName, item.classification.score]).sort((a, b) => a[1] > b[1])
-				console.log(results)
-				handleResults(results)
-			}).catch(err => console.log(err));
-		});
+    event.preventDefault();
 
-	// Nav.
-		var $nav = $('#nav');
+    fetch(
+      "https://automl.googleapis.com/v1beta1/projects/795970644708/locations/us-central1/models/TCN490914349615939584:predict",
+      {
+        body: JSON.stringify({
+          payload: {
+            textSnippet: {
+              content: $("#text-area").val(),
+              mime_type: "text/plain"
+            }
+          }
+        }),
+        headers: {
+          Authorization: "Bearer " + accessToken,
+          "Content-Type": "application/json"
+        },
+        method: "POST"
+      }
+    )
+      .then(res => res.json())
+      .then(res => {
+        var results = res.payload
+          .map(item => [item.displayName, item.classification.score])
+          .sort((a, b) => a[1] > b[1]);
+				console.log(results);
+				handleResults(results);
+				$([document.documentElement, document.body]).animate({
+					scrollTop: $("#first").offset().top
+				}, 1000)
+      })
+      .catch(err => console.log(err));
+  });
 
-		if ($nav.length > 0) {
+  // Nav.
+  var $nav = $("#nav");
 
-			// Shrink effect.
-				$main
-					.scrollex({
-						mode: 'top',
-						enter: function() {
-							$nav.addClass('alt');
-						},
-						leave: function() {
-							$nav.removeClass('alt');
-						},
-					});
+  if ($nav.length > 0) {
+    // Shrink effect.
+    $main.scrollex({
+      mode: "top",
+      enter: function() {
+        $nav.addClass("alt");
+      },
+      leave: function() {
+        $nav.removeClass("alt");
+      }
+    });
 
-			// Links.
-				var $nav_a = $nav.find('a');
+    // Links.
+    var $nav_a = $nav.find("a");
 
-				$nav_a
-					.scrolly({
-						speed: 1000,
-						offset: function() { return $nav.height(); }
-					})
-					.on('click', function() {
+    $nav_a
+      .scrolly({
+        speed: 1000,
+        offset: function() {
+          return $nav.height();
+        }
+      })
+      .on("click", function() {
+        var $this = $(this);
 
-						var $this = $(this);
+        // External link? Bail.
+        if ($this.attr("href").charAt(0) != "#") return;
 
-						// External link? Bail.
-							if ($this.attr('href').charAt(0) != '#')
-								return;
+        // Deactivate all links.
+        $nav_a.removeClass("active").removeClass("active-locked");
 
-						// Deactivate all links.
-							$nav_a
-								.removeClass('active')
-								.removeClass('active-locked');
+        // Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
+        $this.addClass("active").addClass("active-locked");
+      })
+      .each(function() {
+        var $this = $(this),
+          id = $this.attr("href"),
+          $section = $(id);
 
-						// Activate link *and* lock it (so Scrollex doesn't try to activate other links as we're scrolling to this one's section).
-							$this
-								.addClass('active')
-								.addClass('active-locked');
+        // No section for this link? Bail.
+        if ($section.length < 1) return;
 
-					})
-					.each(function() {
+        // Scrollex.
+        $section.scrollex({
+          mode: "middle",
+          initialize: function() {
+            // Deactivate section.
+            if (browser.canUse("transition")) $section.addClass("inactive");
+          },
+          enter: function() {
+            // Activate section.
+            $section.removeClass("inactive");
 
-						var	$this = $(this),
-							id = $this.attr('href'),
-							$section = $(id);
+            // No locked links? Deactivate all links and activate this section's one.
+            if ($nav_a.filter(".active-locked").length == 0) {
+              $nav_a.removeClass("active");
+              $this.addClass("active");
+            }
 
-						// No section for this link? Bail.
-							if ($section.length < 1)
-								return;
+            // Otherwise, if this section's link is the one that's locked, unlock it.
+            else if ($this.hasClass("active-locked"))
+              $this.removeClass("active-locked");
+          }
+        });
+      });
+  }
 
-						// Scrollex.
-							$section.scrollex({
-								mode: 'middle',
-								initialize: function() {
-
-									// Deactivate section.
-										if (browser.canUse('transition'))
-											$section.addClass('inactive');
-
-								},
-								enter: function() {
-
-									// Activate section.
-										$section.removeClass('inactive');
-
-									// No locked links? Deactivate all links and activate this section's one.
-										if ($nav_a.filter('.active-locked').length == 0) {
-
-											$nav_a.removeClass('active');
-											$this.addClass('active');
-
-										}
-
-									// Otherwise, if this section's link is the one that's locked, unlock it.
-										else if ($this.hasClass('active-locked'))
-											$this.removeClass('active-locked');
-
-								}
-							});
-
-					});
-
-		}
-
-	// Scrolly.
-		$('.scrolly').scrolly({
-			speed: 1000
-		});
-
+  // Scrolly.
+  $(".scrolly").scrolly({
+    speed: 1000
+  });
 })(jQuery);
